5a21a4372678ecd6d4fbc03a6cde6ed75a3ee556
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2019-03-16 17:10:08 -0400

    created increase and decrease counter methods

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,16 +2,31 @@ import React, { Component } from "react";
 import { View, Text, StyleShee, TouchableOpacity } from "react-native";
 
 class Counter extends Component {
-  state = {};
+  state = {
+    counter: 0
+  };
+
+  increaseCounter = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  };
+
+  decreaseCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
   render() {
     return (
       <View style={styles.root}>
         <View style={styles.counter}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => this.increaseCounter}>
             <Text>Increase</Text>
           </TouchableOpacity>
-          <Text style={styles.counterText}>0</Text>
-          <TouchableOpacity>
+          <Text style={styles.counterText}>{this.state.counter}</Text>
+          <TouchableOpacity onPress={() => this.decreaseCounter}>
             <Text style={styles.counterText}>Decrease</Text>
           </TouchableOpacity>
         </View>
